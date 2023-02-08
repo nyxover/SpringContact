c88fd31458c5b068c3ba9d7b81f19b4bf24d94e7
Pierre Buirette <buirette.pierre@gmail.com>
2023-02-08 14:15:00 +0100

    Creation du projet avec Spring initialzr

---
+++ src/main/java/com/springcontact/springcontact/SpringcontactApplication.java
@@ -0,0 +1,13 @@
+package com.springcontact.springcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcontactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcontact/springcontact/SpringcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcontact.springcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
