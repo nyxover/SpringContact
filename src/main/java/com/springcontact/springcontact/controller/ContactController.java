45ecdf380f91d4c1594302e84e7cf5b18156f598
hzouba97 <hamid59950@hotmail.com>
2023-02-08 15:37:11 +0100

    Creation du ContactController

---
+++ src/main/java/com/springcontact/springcontact/controller/ContactController.java
@@ -0,0 +1,52 @@
+package com.springcontact.springcontact.controller;
+
+import com.springcontact.springcontact.repository.entity.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class ContactController {
+
+
+    @Autowired
+    private ContactService contactService;
+
+
+    @GetMapping("/all")
+    public String displayAllContacts(Model model) {
+        List<ContactDto> contactList = contactService.fetchContacts();
+        model.addAttribute("contacts", contactList);
+        return "home.html";
+    }
+
+    @GetMapping("/{id}")
+    public  String displaySpecificContact(@PathVariable Long id, Model model){
+        Optional<ContactDto> contactOptional = contactService.fetchById(id);
+        if (contactOptional.isPresent()){
+            model.addAttribute("contact", contactOptional.get());
+            return "contact-details.html";
+        } else {
+            return "404.html";
+        }
+    }
+
+    @GetMapping ("/add")
+    public String displayAddContactForm(Model model) {
+        Contact contact = new Contact();
+        model.addAttribute("contact", contact);
+        return "add-contact";
+    }
+
+    @PostMapping("/add")
+    public String addContactFormSubmission(Contact contact, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "add-contact";
+        }
+        contactService.addContact(contact);
+        return "redirect:/contact/all";
+    }
+
+}
