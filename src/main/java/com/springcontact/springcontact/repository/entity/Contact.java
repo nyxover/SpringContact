bf6bf60d073a8890fa423817b8547afc9ea9952b
hzouba97 <hamid59950@hotmail.com>
2023-02-08 15:20:02 +0100

    Creation class Contact

--- src/main/java/com/springcontact/springcontact/repository/entity/Contact.java
+++ src/main/java/com/springcontact/springcontact/repository/entity/Contact.java
@@ -1,4 +1,64 @@
 package com.springcontact.springcontact.repository.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+
+    @NotBlank
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
