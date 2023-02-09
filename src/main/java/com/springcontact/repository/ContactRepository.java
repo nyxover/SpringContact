fc755d68f2225528b40969c221751efad5ea8826
hzouba97 <hamid59950@hotmail.com>
2023-02-09 10:48:51 +0100

    Corrections messages d'erreurs

--- src/main/java/com/springcontact/repository/ContactRepository.java
+++ src/main/java/com/springcontact/repository/ContactRepository.java
@@ -2,7 +2,9 @@ package com.springcontact.repository;
 
 import com.springcontact.repository.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
 }
--- src/main/java/com/springcontact/repository/entity/Contact.java
+++ src/main/java/com/springcontact/repository/entity/Contact.java
@@ -1,16 +1,17 @@
 package com.springcontact.repository.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO )
     private Long id;
 
-    @NotBlank
+
     private String first_name;
 
     private String last_name;
